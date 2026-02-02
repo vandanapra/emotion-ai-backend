74288a125189ca5de9d715cabb100ecef8caf8c4
vandanapra <vandanaiec3093@gmail.com>
2026-02-02 14:09:36 +0530

    fix

--- app/main.py
+++ app/main.py
@@ -4,26 +4,14 @@ from app.emotion_model import detect_emotion
 from app.risk_engine import calculate_risk
 from sqlalchemy.orm import Session
 from fastapi import Depends
-
+from typing import List
+from app.schemas import EmotionHistoryResponse
 from app.database import get_db
 from app.models import EmotionHistory
 
 app = FastAPI()
 
-# @app.post("/detect-emotion", response_model=EmotionResponse)
-# def detect_emotion_api(request: EmotionRequest):
-#     emotion_data = detect_emotion(request.text)
 
-#     risk = calculate_risk(
-#         emotion=emotion_data["emotion"],
-#         confidence=emotion_data["confidence"]
-#     )
-
-#     return {
-#         "emotion": emotion_data["emotion"],
-#         "confidence": emotion_data["confidence"],
-#         "risk_level": risk
-#     }
 
 @app.post("/detect-emotion", response_model=EmotionResponse)
 def detect_emotion_api(
@@ -52,3 +40,14 @@ def detect_emotion_api(
         "confidence": emotion_data["confidence"],
         "risk_level": risk
     }
+    
+@app.get("/emotion-history", response_model=List[EmotionHistoryResponse])
+def get_emotion_history(db: Session = Depends(get_db)):
+    records = (
+        db.query(EmotionHistory)
+        .order_by(EmotionHistory.created_at.desc())
+        .limit(20)
+        .all()
+    )
+    return records
+
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class EmotionRequest(BaseModel):
     text: str
@@ -7,3 +8,13 @@ class EmotionResponse(BaseModel):
     emotion: str
     confidence: float
     risk_level: str
+    
+class EmotionHistoryResponse(BaseModel):
+    text: str
+    emotion: str
+    confidence: float
+    risk_level: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
